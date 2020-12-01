dc0d569eadad31297068930dbafe7f83f9b5b561
AlexanderMessing <75305970+AlexanderMessing@users.noreply.github.com>
2020-12-01 13:42:02 +0100

    test123

---
+++ hallo-github/hallo-github/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hallo_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
