0b3ce771aa1c4b0e1323170d0b2499954ca5a56b
AlexanderMessing <75305970+AlexanderMessing@users.noreply.github.com>
2020-12-01 14:54:40 +0100

    neu

--- hallo-github/hallo-github/Program.cs
+++ hallo-github/hallo-github/Program.cs
@@ -7,6 +7,7 @@ namespace hallo_github
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Test!");
         }
     }
 }
